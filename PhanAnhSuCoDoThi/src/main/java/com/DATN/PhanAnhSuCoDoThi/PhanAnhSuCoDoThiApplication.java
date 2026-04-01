754d378fae87e81c64771bea9393471023898d11
Phạm Thiện Tâm <22115053122336@sv.ute.udn.vn>
2026-04-01 17:13:44 +0700

    first commit

---
+++ PhanAnhSuCoDoThi/src/main/java/com/DATN/PhanAnhSuCoDoThi/PhanAnhSuCoDoThiApplication.java
@@ -0,0 +1,13 @@
+package com.DATN.PhanAnhSuCoDoThi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhanAnhSuCoDoThiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhanAnhSuCoDoThiApplication.class, args);
+	}
+
+}
---
+++ PhanAnhSuCoDoThi/src/test/java/com/DATN/PhanAnhSuCoDoThi/PhanAnhSuCoDoThiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DATN.PhanAnhSuCoDoThi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanAnhSuCoDoThiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
